1554298cc973693a7585a1c5184129334a465655
Chris Harris <chris.harris@kitware.com>
2014-11-21 14:28:13 -0500

    Add settng validation function

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,18 @@
 import sys
-
+from girder.models.model_base import ValidationException
+from girder import events
 from .proxy import Proxy
 
+import constants
+
+def validate_settings(event):
+    key = event.info['key']
+
+    if key == constants.PluginSettings.PROXY_FILE_PATH:
+        event.preventDefault().stopPropagation()
+
+
+
 def load(info):
+    events.bind('model.setting.validate', 'pvwproxy', validate_settings)
     info['apiRoot'].proxy = Proxy()
