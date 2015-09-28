eccf61218e0cdb033e6f3a07db6fca70ec2e8bd9
Tristan Wright <tristan.wright@kitware.com>
2015-09-28 14:57:09 -0600

    refinements

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,7 +1,10 @@
 angular.module('kw.cmb.app', ['ui.router', 'kitware.cmb', 'kitware.SimPut'])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  var annonymousGoToLogin = ["$state", "$scope", function ($state, $scope) {if($scope.user === null) {$state.go('login');}}],
-    goToHome = ["$state", "$scope", function ($state, $scope) {if($scope.user !== null) {$state.go('home');}}];
+  var goToHome = ["$state", "$scope", function ($state, $scope) {
+        if($scope.user !== null) {
+            $state.go('home');
+        }
+    }];
 
 
   // For any unmatched url, redirect to /state1
