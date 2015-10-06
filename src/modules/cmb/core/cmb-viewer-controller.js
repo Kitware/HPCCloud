14f3adf6c094fc5fca17276a150145d7fe932951
Chris Harris <chris.harris@kitware.com>
2015-10-06 18:21:35 -0400

    Use encodeURIComponent to encode '/' in sessionId

--- src/modules/cmb/core/cmb-viewer-controller.js
+++ src/modules/cmb/core/cmb-viewer-controller.js
@@ -1,8 +1,8 @@
 angular.module("kitware.cmb.core")
     .controller('CmbViewerController', ['$scope', '$state', '$stateParams', '$window', function ($scope, $state, $stateParams, $window) {
         var hostPort = $window.location.host;
-       	hostPort = 'ulex:8080';
-        $scope.connectionURL = ($stateParams.mode === 'launcher') ? "/paraview" : "ws://"+hostPort+"/proxy?sessionId=" + $stateParams.sessionId;
+         hostPort = 'ulex';
+        $scope.connectionURL = ($stateParams.mode === 'launcher') ? "/paraview" : ("ws://"+hostPort+"/proxy?sessionId=" + encodeURIComponent($stateParams.sessionId));
         // $scope.connectionURL = 'ws://ulex:8080/ws';
         $scope.itemId = $stateParams.simulationID;
 
