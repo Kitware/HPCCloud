8605ab8f5d1b5fe7161c49a1f76c18bfe92cfa60
TristanWright <tristan.wright@kitware.com>
2015-10-13 10:13:15 -0600

    fix #82 ulex is hardcoded as websock hostPort!

--- src/modules/cmb/core/cmb-viewer-controller.js
+++ src/modules/cmb/core/cmb-viewer-controller.js
@@ -2,10 +2,6 @@ angular.module("kitware.cmb.core")
     .controller('CmbViewerController', ['$scope', '$stateParams', '$window',
     	function ($scope, $stateParams, $window) {
         var hostPort = $window.location.host;
-         hostPort = 'ulex';
         $scope.connectionURL = ($stateParams.mode === 'launcher') ? "/paraview" : ("ws://"+hostPort+"/proxy?sessionId=" + encodeURIComponent($stateParams.sessionId));
-        // $scope.connectionURL = 'ws://ulex:8080/ws';
         $scope.itemId = $stateParams.simulationID;
-
-        //cluster / jobId
     }]);
