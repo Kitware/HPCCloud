3fb01c60c3dcf990543508e3e8a9e7397a094978
Chris Harris <chris.harris@kitware.com>
2016-02-16 11:39:20 -0500

    Add export step to PyFR taskflow
    Export the solution files to VTK file format

--- server/taskflows/hpccloud/taskflow/pyfr.py
+++ server/taskflows/hpccloud/taskflow/pyfr.py
@@ -116,6 +116,17 @@ def _partition_mesh(logger, input_path, output_dir,  n):
         logger.exception(ex.output)
         raise
 
+def _export_solution(logger, mesh_path, input_path, output_path):
+    command = [
+        'pyfr', 'export', mesh_path, input_path, output_path
+    ]
+
+    try:
+        subprocess.check_output(command, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as ex:
+        logger.exception(ex.output)
+        raise
+
 @cumulus.taskflow.task
 def pyfr_terminate(task):
     cluster = task.taskflow['cluster']
@@ -154,21 +165,22 @@ def import_mesh(task, *args, **kwargs):
 
         if 'numberOfSlots' in kwargs \
                 and int(kwargs['numberOfSlots']) > 1:
-            print "partitioning: %s" % kwargs['numberOfSlots']
             _partition_mesh(
                 task.logger, output_path, output_dir, kwargs['numberOfSlots'])
+
+
         else:
             task.logger.info('Skipping partitioning we are running serial.')
-
 
         task.logger.info('Partitioning complete.')
 
         task.logger.info('Uploading converted mesh.')
-        for f in os.listdir(output_dir):
-            path = os.path.join(output_dir, f)
-            size = os.path.getsize(path)
-            with open(path) as fp:
-                client.uploadFile(input_folder_id, fp, f, size=size, parentType='folder')
+        size = os.path.getsize(output_path)
+        with open(output_path) as fp:
+            girder_file = client.uploadFile(
+                input_folder_id, fp, 'mesh.pyfrm', size=size,
+                parentType='folder')
+            kwargs['imported_mesh_file_id'] = girder_file['_id']
 
         task.logger.info('Upload complete.')
 
@@ -247,6 +259,29 @@ def monitor_pyfr_job(task, cluster, job, *args, **kwargs):
     task.taskflow.run_task(
         monitor_job.s(cluster, job, girder_token=girder_token))
 
+NUMBER__OF_EXPORT_TASKS = 10
+
+def _list_solution_files(client, folder_id):
+    """
+    List the PyFR solution files held in a Girder folder.
+
+    :param client: The Girder client for Girder access.
+    :param folder_id: The folder to list solution files in.
+    """
+
+    items = client.listItem(folder_id)
+    for item in items:
+        files = client.get('item/%s/files' % item['_id'], parameters={
+            'limit': 1,
+        })
+
+        if len(files) == 0:
+            continue
+
+        file = files[0]
+        exts = file['exts']
+        if len(exts) == 2 and exts[1] == 'pyfrs':
+            yield file
 
 @cumulus.taskflow.task
 def upload_output(task, cluster, job, *args, **kwargs):
@@ -263,4 +298,72 @@ def upload_output(task, cluster, job, *args, **kwargs):
 
     task.taskflow.logger.info('Upload complete.')
 
+    # Now we need to export to VTK format
+    imported_mesh_file_id = kwargs.pop('imported_mesh_file_id')
 
+    client = _create_girder_client(
+        task.taskflow.girder_api_url, task.taskflow.girder_token)
+    solution_files = list(_list_solution_files(client, output_folder_id))
+    number_files = len(solution_files)
+
+    # The number 100 is pretty arbitrary!
+    if number_files < 100:
+        export_output.delay(
+            output_folder_id, imported_mesh_file_id, solution_files)
+    # Break into chunks a run in parallel
+    else:
+        for chunk in [solution_files[i::NUMBER__OF_EXPORT_TASKS] for i in xrange(NUMBER__OF_EXPORT_TASKS)]:
+            export_output.delay(output_folder_id, imported_mesh_file_id, chunk)
+
+
+def _export_file(task, client, output_folder_id, mesh_path, file, output_dir):
+    """
+    Export a single solution file held in Girder into VTK format
+
+    :param task: The current task we are running in.
+    :param client: The Girder client for access to Girder.
+    :param output_folder_id: The target folder to upload the VTK file to.
+    :param meth_path: The path to the mesh file.
+    :param file: The Girder file object for the solution file.
+    :param output_dir: The temporay working directory to write files into.
+    """
+    name = file['name']
+    solution_file_path = os.path.join(output_dir, name)
+    client.downloadFile(file['_id'], solution_file_path)
+    vtk_filename = '%s.vtu' % name.rsplit('.', 1)[0]
+    vtk_file_path = os.path.join(output_dir, vtk_filename)
+    _export_solution(task.logger, mesh_path, solution_file_path, vtk_file_path)
+    with open(vtk_file_path, 'r') as fp:
+        size = os.path.getsize(vtk_file_path)
+        client.uploadFile(
+            output_folder_id, fp, vtk_filename, size=size,
+            parentType='folder')
+
+
+@cumulus.taskflow.task
+def export_output(task, folder_id, imported_mesh_file_id, files):
+    """
+    Export a batch of PyFR solution files into VTK format.
+
+    :param: folder_id: The target folder id to upload the VTK files to.
+    :param: imported_mesh_file_id: The mesh in PyFR format
+    :param: files: The files to export ( Girder JSON objects )
+    """
+    client = _create_girder_client(
+        task.taskflow.girder_api_url, task.taskflow.girder_token)
+
+    output_dir =  tempfile.mkdtemp()
+
+    try:
+        mesh_path = os.path.join(output_dir, 'mesh.pyfrm')
+
+        task.logger.info('Downloading mesh.')
+        client.downloadFile(imported_mesh_file_id, mesh_path)
+        task.logger.info('Downloading complete.')
+
+        for file in files:
+            _export_file(task, client, folder_id, mesh_path, file, output_dir)
+
+    finally:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
