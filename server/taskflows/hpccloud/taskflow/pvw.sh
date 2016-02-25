ca3eb0632ece101b06ad2da335b5921de6175700
Chris Harris <chris.harris@kitware.com>
2016-02-25 16:31:37 -0500

    Update ParaView version information
    This needs to me made more generic

--- server/taskflows/hpccloud/taskflow/pvw.sh
+++ server/taskflows/hpccloud/taskflow/pvw.sh
@@ -6,7 +6,7 @@
 # Set up cluster-specific variables
 PARAVIEW_DIR={{paraviewInstallDir if paraviewInstallDir else "/opt/paraview/install"}}
 PV_PYTHON="${PARAVIEW_DIR}/bin/pvpython"
-LIB_VERSION_DIR=`ls ${PARAVIEW_DIR}/lib | grep paraview`
+LIB_VERSION_DIR=paraview-5.0
 APPS_DIR="lib/${LIB_VERSION_DIR}/site-packages/paraview/web"
 VISUALIZER="${PARAVIEW_DIR}/${APPS_DIR}/pv_web_visualizer.py"
 GET_PORT_PYTHON_CMD='import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'
@@ -39,7 +39,8 @@ KEY="{{ sessionKey }}"
 BODY='{"host": "'$IPADDRESS'", "port": '${WEBSOCKET_PORT}', "key": "'$KEY'"}'
 curl --silent --show-error -o /dev/null -X POST -d "$BODY"  --header "Content-Type: application/json" {{ baseUrl }}/proxy
 
-export LD_LIBRARY_PATH=${PARAVIEW_DIR}/lib/${LIB_VERSION_DIR}
+export LD_LIBRARY_PATH=${PARAVIEW_DIR}/lib/${LIB_VERSION_DIR}:
+export PYTHONPATH=/opt/paraview/lib/paraview-4.4/site-packages/vtk/
 export DISPLAY=:0
 
 # First run pvpython
