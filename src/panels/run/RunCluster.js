0f21c6cab699b5968ed9904573da6710a7a2ce05
TristanWright <tristan.wright@kitware.com>
2016-04-21 22:55:14 -0400

    guards for some spots spots that can be undefined

--- src/panels/run/RunCluster.js
+++ src/panels/run/RunCluster.js
@@ -38,7 +38,7 @@ export default React.createClass({
     };
   },
 
-  componentWillMount() {
+  componentDidMount() {
     this.updateState();
   },
 
--- src/workflows/pyfr/common/steps/Simulation/View/index.js
+++ src/workflows/pyfr/common/steps/Simulation/View/index.js
@@ -102,7 +102,7 @@ const SimualtionView = React.createClass({
     }
 
     const { taskflow, taskflowId, simulation, buttonsDisabled, error } = this.props;
-    const actions = [].concat(taskflow.actions);
+    const actions = [].concat(taskflow.actions ? taskflow.actions : []);
 
     // Extract meaningful information from props
     if (taskflow.allComplete) {
