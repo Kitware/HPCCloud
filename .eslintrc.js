c44942e973da4378c7a68413fb6d780ee2fb66bb
TristanWright <tristan.wright@kitware.com>
2016-05-10 14:20:45 -0600

    latest kw-web-suite, adjusted lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,17 @@ module.exports = {
     'no-param-reassign': [2, { props: false }],
     'no-unused-vars': [2, { args: 'none' }],
     'no-var': 0,
+    'no-underscore-dangle': 0,
     'one-var': 0,
+    'block-spacing': 0,
+    'global-require': 0,
+    'import/no-unresolved': 0,
     'react/no-is-mounted': 1,
     'react/prefer-es6-class': 0,
     'react/prefer-stateless-function': 0,
+    'react/jsx-curly-spacing': 0,
+    'react/jsx-first-prop-new-line': 0,
+    'react/jsx-indent': 0,
+    'jsx-a11y/img-has-alt': 0,
   }
 };
