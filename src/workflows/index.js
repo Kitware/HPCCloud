6b736aaa7572b68bc943aea2189f71224f15e7b3
TristanWright <tristan.wright@kitware.com>
2016-07-29 11:03:40 -0600

    removed code saturn from workflows, fix #464

--- src/workflows/index.js
+++ src/workflows/index.js
@@ -1,10 +1,10 @@
-import CodeSaturn   from './code-saturn';
+// import CodeSaturn   from './code-saturn';
 import PyFr         from './pyfr/pyfr-simput';
 import PyFrExec     from './pyfr/pyfr-exec';
 import Visualizer   from './visualizer';
 
 const Workflows = {
-  CodeSaturn,
+  // CodeSaturn,
   PyFr,
   PyFrExec,
   Visualizer,
