9d9fb3630b6905043a12e41308861c1bbf2361a9
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-01-20 11:35:39 -0700

    Clean nwchem-exec

--- src/workflows/nwchem/nwchem-exec/components/root/NewSimulation.js
+++ src/workflows/nwchem/nwchem-exec/components/root/NewSimulation.js
@@ -1,19 +1,24 @@
 import React                from 'react';
 import { FileUploadEntry }  from '../../../../../panels/ItemEditor';
 
-export default React.createClass({
+export default function newSimulation(props) {
+  return (
+    <div>
+        <FileUploadEntry
+          name="geometry"
+          label="Geometry file"
+          accept=".pdb,.xyz"
+          owner={ props.owner }
+        />
+        <FileUploadEntry
+          name="nw"
+          label="nw file"
+          accept=".nw"
+          owner={ props.owner }
+        />
+    </div>);
+}
 
-  displayName: 'Simulation/New/NWChemExec',
-
-  propTypes: {
-    owner: React.PropTypes.func,
-  },
-
-  render() {
-    return (
-      <div>
-        <FileUploadEntry name="geometry" label="Geometry file" accept=".pdb,.xyz" owner={ this.props.owner } />
-        <FileUploadEntry name="nw" label="nw file" accept=".nw" owner={ this.props.owner } />
-      </div>);
-  },
-});
+newSimulation.propTypes = {
+  owner: React.PropTypes.func,
+};
